5e9a98e659e6458fa828e94928099dbe9e540fa7
Stormtroppy <150097596+Stormtroppy@users.noreply.github.com>
2025-07-09 18:06:12 -0400

    Enhance layout and interactivity: add Font Awesome icons for contact, improve CSS styles for links and headings, and refactor JavaScript for better readability.

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-let burger = document.querySelector(".burger-menu");
-let menu = document.querySelector(".main-nav");
+const burger = document.querySelector(".burger-menu");
+const menu = document.querySelector(".main-nav");
+const linkedin = document.querySelector(".fa-linkedin");
+const email = document.querySelector(".fa-envelope");
+
 burger.addEventListener("click", function () {
   burger.classList.toggle("active");
   menu.classList.toggle("active");
+});
+linkedin.addEventListener("click", function () {
+  window.location.href = "https://www.linkedin.com/";
+});
+email.addEventListener("click", function () {
+  window.location.href = "mailto:Stormtroppy@example.com";
 });
